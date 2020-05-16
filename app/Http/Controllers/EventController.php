a3aa151a78830f2f5ba4790c02e5fe5358cd8701
DaniCristante <dcristan18@jaumebalmes.net>
2020-05-16 14:21:27 +0200

    getting sports for event create form

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -2,29 +2,32 @@
 
 namespace App\Http\Controllers;
 
+use App\ApiHandlers\CallHandler;
 use Illuminate\Http\Request;
 
 class EventController extends Controller
 {
+    private $callHandler;
+    public function __construct(CallHandler $callHandler)
+    {
+        $this->callHandler = $callHandler;
+    }
+
     /***  RETURN VIEW AND CREATE EVENT   */
     public function createEvent()
     {
-
         //TODO des de la api necessitari un get con todas las categorias y sus id's
+        $response = $this->callHandler->unauthorizedGetMethodHandler('/sports');
+        $sports = [];
 
-        //Test
-        $dep1 = [
-            'id' => 1,
-            'name' => 'Fútbol'
-        ];
-        $dep2 = [
-            'id' => 2,
-            'name' => 'Natación'
-        ];
-        $categorias = array($dep1, $dep2);
+        foreach ($response as $sport){
+            array_push($sports, [
+                'id' => $sport['id'],
+                'name' => $sport['name']
+            ]);
+        }
 
-
-        return view('wesports.events.create', array('categorias' => $categorias));
+        return view('wesports.events.create', array('sports' => $sports));
     }
 
 
