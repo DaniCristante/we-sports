c59687eb5cf331102bb5551c1d26026e4214aa08
DanielAagaard98 <daagaard18@jaumebalmes.net>
2020-05-25 20:29:18 +0200

    header responsive

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -9,54 +9,57 @@
         <div class="row align-items-center">
             {{--Only homepage cover header--}}
             <header id="homePageHeader" class="container-fluid  p-2 bg-dark">
-                <div class="col-12">
-                    <h2 class="text-center">Bienvenido a WeSports</h2>
-                    <div id="web-abstract">
-                        <h4 class="p-2">
-                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus alias dolorem eligendi
-                            error
-                            eum
-                            harum ipsam libero, modi molestias obcaecati odit pariatur perferendis quidem rem sit,
-                            soluta
-                            totam.
-                            Beatae, officiis.
-                        </h4>
+                <div class="container my-4">
+                    <div class="col-12">
+                        <h2 class="text-center">Bienvenido a WeSports</h2>
+                        <div id="web-abstract">
+                            <h4 class="p-2">
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus alias dolorem
+                                eligendi
+                                error
+                                eum
+                                harum ipsam libero, modi molestias obcaecati odit pariatur perferendis quidem rem sit,
+                                soluta
+                                totam.
+                                Beatae, officiis.
+                            </h4>
+                        </div>
                     </div>
-                </div>
-                <div class="container">
-                    <div class="">
-                        <h4 class="text-center">Filtra por ciudad, fecha o deporte</h4>
-                    </div>
-                    <form action="{{url('events')}}" method="post" novalidate="novalidate">
-                        @csrf
-                        <div class="row">
-                            <div class="col-lg-12">
-                                <div class="row">
-                                    <div class="col-lg-3 col-md-3 col-sm-12 p-0">
-                                        <input type="text" name="city" class="form-control search-slt"
-                                               placeholder="Ciudad">
-                                    </div>
-                                    <div class="col-lg-3 col-md-3 col-sm-12 p-0">
-                                        <input type="date" name="date" class="form-control search-slt"
-                                               placeholder="Fecha">
-                                    </div>
-                                    <div class="col-lg-3 col-md-3 col-sm-12 p-0">
-                                        <select class="form-control search-slt" name="sport" id="sportsList">
-                                            <option value="none" disabled hidden selected>- Selecciona un deporte -
-                                            </option>
-                                            @foreach($sports as $sport)
-                                                <option value="{{$sport['id']}}">{{$sport['name']}}</option>
-                                            @endforeach
-                                        </select>
-                                    </div>
-                                    <div class="col-lg-3 col-md-3 col-sm-12 p-0">
-                                        <input type="submit" name="submit" class="btn btn-secondary wrn-btn"
-                                               value="Buscar">
+                    <div class="container">
+                        <div class="">
+                            <h4 class="text-center">Filtra por ciudad, fecha o deporte</h4>
+                        </div>
+                        <form action="{{url('events')}}" method="post" novalidate="novalidate">
+                            @csrf
+                            <div class="row">
+                                <div class="col-lg-12">
+                                    <div class="row">
+                                        <div class="col-lg-3 col-md-3 col-sm-12 p-0">
+                                            <input type="text" name="city" class="form-control search-slt"
+                                                   placeholder="Ciudad">
+                                        </div>
+                                        <div class="col-lg-3 col-md-3 col-sm-12 p-0">
+                                            <input type="date" name="date" class="form-control search-slt"
+                                                   placeholder="Fecha">
+                                        </div>
+                                        <div class="col-lg-3 col-md-3 col-sm-12 p-0">
+                                            <select class="form-control search-slt" name="sport" id="sportsList">
+                                                <option value="none" disabled hidden selected>- Selecciona un deporte -
+                                                </option>
+                                                @foreach($sports as $sport)
+                                                    <option value="{{$sport['id']}}">{{$sport['name']}}</option>
+                                                @endforeach
+                                            </select>
+                                        </div>
+                                        <div class="col-lg-3 col-md-3 col-sm-12 p-0">
+                                            <input type="submit" name="submit" class="btn btn-secondary wrn-btn"
+                                                   value="Buscar">
+                                        </div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                    </form>
+                        </form>
+                    </div>
                 </div>
             </header>
         </div>
@@ -64,9 +67,9 @@
     @if ($message = Session::get('created-event'))
         <h3 class="alert alert-success">{{$message}}</h3>
     @endif
-    <div id="homePageContent">
+    <div id="homePageContent" class="mt-4">
         <div id="page-title">
-            <h1 class="text-center">Eventos con más participantes</h1>
+            <h2 class="text-center h1 text-dark">Eventos con más participantes</h2>
         </div>
         @include('wesports.events.event-list')
 
