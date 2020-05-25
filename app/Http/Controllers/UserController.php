5e5324fb5f62d75ba4891034b9830f1aee6cb27f
DanielAagaard98 <daagaard18@jaumebalmes.net>
2020-05-25 12:48:09 +0200

    user controller

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\ApiHandlers\CallHandler;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    private $callHandler;
+
+    public function __construct(CallHandler $callHandler)
+    {
+        $this->callHandler = $callHandler;
+    }
+
+    public function getProfile()
+    {
+        $url = url()->current();
+        $pos_nickname = strrpos($url, "/", 0);
+        $nickname = substr($url, $pos_nickname + 1, strlen($url));
+        $requestUrl = '/profile/' . $nickname;
+
+        dump($requestUrl);
+        die();
+
+
+    }
+}
