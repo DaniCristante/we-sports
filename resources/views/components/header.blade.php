c5671c49a8c23b0c81f4cede3d4e6205c7e5e6c2
DanielAagaard98 <daagaard18@jaumebalmes.net>
2020-05-29 23:13:58 +0200

    retoques finales

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,13 +1,57 @@
-<nav class="navbar navbar-expand-lg bg-dark sticky-top">
-    <div class="col-6">
-        <a class="navbar-brand" href="http://localhost/weSports/we-sports/public/">
-            <img class="border border-primary rounded-lg" src="{{asset('images/brand-logo.png')}}" alt="WeSports"
-                 width="180" height="40">
-        </a>
+<div class="container-fluid">
+    <div class="row align-items-center">
+        <header id="homePageHeader" class="container-fluid  p-2 bg-dark">
+            @if (session('status'))
+                <div id="alerts" class="alert alert-success text-center">
+                    {{session('status')}}
+                </div>
+            @endif
+            <div class="container my-4">
+                <div class="col-12">
+                    <h2 class="text-center">Bienvenido a WeSports</h2>
+                    <div id="web-abstract">
+                        <h4 class="p-2 text-center border-bottom">
+                            Crea eventos, únete a actividades y comparte experiencias.
+                        </h4>
+                    </div>
+                </div>
+                <div class="container">
+                    <div class="">
+                        <h5 class="text-center text-white">Filtra por ciudad, fecha o deporte</h5>
+                    </div>
+                    <form action="{{url('events')}}" method="post" novalidate="novalidate">
+                        @csrf
+                        <div class="row">
+                            <div class="col-lg-12">
+                                <div class="row">
+                                    <div class="col-lg-3 col-md-3 col-sm-12 p-0 my-1">
+                                        <input type="text" name="city" class="form-control search-slt"
+                                               placeholder="Ciudad">
+                                    </div>
+                                    <div class="col-lg-3 col-md-3 col-sm-12 p-0 my-1">
+                                        <input type="date" name="date" class="form-control search-slt"
+                                               placeholder="Fecha">
+                                    </div>
+                                    <div class="col-lg-3 col-md-3 col-sm-12 p-0 my-1">
+                                        <select class="form-control search-slt" name="sport" id="sportsList">
+                                            <option value="none" disabled hidden selected>- Selecciona un deporte -
+                                            </option>
+                                            @foreach($sports as $sport)
+                                                <option value="{{$sport['id']}}">{{$sport['name']}}</option>
+                                            @endforeach
+                                        </select>
+                                    </div>
+                                    <div class="col-lg-3 col-md-3 col-sm-12 p-0 my-1">
+                                        <input type="submit" name="submit" class="btn btn-secondary wrn-btn"
+                                               value="Buscar">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </header>
     </div>
-    <div class="col-4 col-md-6 text-right">
-        <a class="btn btn-outline-light" href="{{url('welcome')}}">Iniciar sesión</a>
-    </div>
-</nav>
-
+</div>
 
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,56 +17,7 @@
 </head>
 <body>
 <div id="app">
-
-    <nav class="navbar navbar-expand-lg bg-dark sticky-top justify-content-between" id="mainMenu">
-        <a class="navbar-brand" href="{{url('/')}}">
-            <img class="border border-primary rounded-lg" src="{{asset('images/brand-logo.png')}}"
-                 alt="WeSports"
-                 width="180" height="40">
-        </a>
-        <button class="navbar-toggler bg-secondary text-dark" type="button" data-toggle="collapse"
-                data-target="#navbarMenuContent"
-                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <i class="fas fa-bars text-dark"></i>
-        </button>
-
-        <div class="collapse navbar-collapse justify-content-end" id="navbarMenuContent">
-            @guest
-
-                <div class="nav-item text-center">
-                    <a href="{{ route('register') }}" class=" text-decoration-none mx-1">
-                        <i class="fas fa-user-plus"> <span class="mx-1">Regístrate</span></i>
-                    </a>
-                    <a href="{{ route('login') }}" class="text-success text-decoration-none mx-1">
-                        <i class="fas fa-user"> <span class="mx-1">Iniciar sessión</span></i>
-                    </a>
-                </div>
-
-        </div>
-
-        @else
-
-            <div class="nav-item" id="navMenuItems">
-
-                <ul class="navbar-nav mr-auto text-white">
-                    <a class="dropdown-item" href="{{url('/dashboard')}}"><i class="fas fa-user-cog"></i>Gestión de cuenta y eventos</a>
-                    <a class="dropdown-item" href="{{url('/profile/'.Auth::user()->nickname)}}"><i class="fas fa-user"></i>Ver mi
-                        perfil</a>
-                    <a class="dropdown-item" href="{{url('/events/create')}}"><i class="fas fa-plus"></i>Crear evento</a>
-                    <a class="dropdown-item" href="{{ route('logout') }}"
-                       onclick="event.preventDefault();
-                                                                     document.getElementById('logout-form').submit();"><i class="fas fa-sign-out-alt"></i>
-                        Cerrar sesión
-                    </a>
-                    <form id="logout-form" action="{{ route('logout') }}" method="POST"
-                          style="display: none;">
-                        @csrf
-                    </form>
-                </ul>
-            </div>
-        @endguest
-
-    </nav>
+    @include('components.nav')
     <main>
         @yield('content')
     </main>
