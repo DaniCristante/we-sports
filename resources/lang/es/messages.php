95645be9d4bf835b60149e3ec5d751665823ac44
DaniCristante <dcristan18@jaumebalmes.net>
2020-05-30 21:54:35 +0200

    homepage navbar es translation

---
+++ resources/lang/es/messages.php
@@ -0,0 +1,23 @@
+<?php
+return [
+    'navbar' => [
+        'my-profile' => 'Ver mi perfil',
+        'create' => 'Crear evento',
+        'manage' => 'Gestión de cuenta y eventos',
+        'logout' => 'Cerrar sesión',
+        'login' => 'Iniciar sesión',
+        'register' => 'Regístrate'
+    ],
+    'homepage' => [
+        'welcome' => 'Bienvenido a WeSports',
+        'abstract' => 'Crea eventos, únete a actividades y comparte experiencias.',
+        'filter' => 'Filtra por ciudad, fecha o deporte',
+        'most-populated' => 'Eventos con más participantes'
+    ],
+    'form-inputs' => [
+        'city' => 'Ciudad',
+        'sport' => 'Selecciona un deporte',
+        'date' => 'Fecha',
+        'find' => 'Buscar',
+    ]
+];
