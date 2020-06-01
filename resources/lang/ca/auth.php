8e5e7d31aa321871a35d25c54f62fe5bc8eeb3af
DanielAagaard98 <daagaard18@jaumebalmes.net>
2020-06-01 21:13:47 +0200

    lang

--- resources/lang/ca/auth.php
+++ resources/lang/ca/auth.php
@@ -1,19 +1,6 @@
 <?php
 
 return [
-
-    /*
-    |--------------------------------------------------------------------------
-    | Authentication Language Lines
-    |--------------------------------------------------------------------------
-    |
-    | The following language lines are used during authentication for various
-    | messages that we need to display to the user. You are free to modify
-    | these language lines according to your application's requirements.
-    |
-    */
-
     'failed' => 'Email i/o contrasenya incorrectes',
     'throttle' => 'Molts intents de login. Si us plau, torni a intentar-ho en :seconds segons.',
-
 ];
