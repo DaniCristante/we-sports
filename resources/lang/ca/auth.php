b9a760419ebf1824ee23d536e7b6998b62bf13c0
DaniCristante <dcristan18@jaumebalmes.net>
2020-06-01 20:22:49 +0200

    auth translations

--- resources/lang/ca/auth.php
+++ resources/lang/ca/auth.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'failed' => 'These credentials do not match our records.',
+    'failed' => 'Email i/o contrasenya incorrectes',
     'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
 
 ];
